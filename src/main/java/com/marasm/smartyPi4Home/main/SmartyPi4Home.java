21c631663d2eeda9911ce33cebbe64c9648e652c
Markas Korotkovas <mkorotkovas@yahoo.com>
2018-02-21 20:29:42 -0700

    testing gpio

--- src/main/java/com/marasm/smartyPi4Home/main/SmartyPi4Home.java
+++ src/main/java/com/marasm/smartyPi4Home/main/SmartyPi4Home.java
@@ -3,8 +3,15 @@
  */
 package com.marasm.smartyPi4Home.main;
 
+import com.marasm.lcd4pi.ButtonPressedObserver;
 import com.marasm.lcd4pi.LCD;
 import com.marasm.logger.AppLogger;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalInput;
+import com.pi4j.io.gpio.PinPullResistance;
+import com.pi4j.io.gpio.RaspiPin;
+import com.pi4j.io.gpio.event.GpioPinListenerDigital;
 
 /**
  * @author mkorotkovas
@@ -23,10 +30,24 @@ public class SmartyPi4Home
       LCD lcd = LCD.getInstance();
       lcd.clear();
       lcd.setText("SmartyPi4Home\nv0.1");
+      
+      ButtonPressedObserver buttonHandler = new ButtonPressedObserver(lcd);
+      buttonHandler.addButtonListener(button -> 
+        System.out.println("Button: " + button.getPin()));
+      
+      
+      GpioController gpio = GpioFactory.getInstance(); 
+      
+      GpioPinDigitalInput rxDataPin = 
+        gpio.provisionDigitalInputPin(RaspiPin.GPIO_27, PinPullResistance.PULL_DOWN);
+      GpioPinListenerDigital rxStateChangeListener = e -> 
+          System.out.println("Pin 27 State changed: " + e.getState().getValue());
+      rxDataPin.addListener( rxStateChangeListener );
+      
     }
     catch (Exception e)
     {
-      AppLogger.error(e.getMessage(), e);
+      AppLogger.error("System Error: " + e.getMessage() + "\nStopping app...", e);
       System.exit(1);
     }
   }
