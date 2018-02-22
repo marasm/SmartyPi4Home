12a08b11f943292ebe4b28cd833706915f35f3c6
Markas Korotkovas <mkorotkovas@yahoo.com>
2018-02-21 20:55:59 -0700

    added the .sh file

---
+++ smartyPi4Home.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo Making sure the current working directory is correct
+cd "$(dirname "$0")" 
+
+echo Getting latest version from GitHub...
+git pull
+git submodule update --recursive --checkout --force --init
+
+echo Launching...
+gradle execute
