cbe1f564fbf797b2aed355297e7da7c55e004e4e
Markas Korotkovas <mkorotkovas@yahoo.com>
2018-02-22 12:44:35 -0700

    updated error handling

--- src/main/java/com/marasm/smartyPi4Home/main/SmartyPi4Home.java
+++ src/main/java/com/marasm/smartyPi4Home/main/SmartyPi4Home.java
@@ -25,37 +25,49 @@ public class SmartyPi4Home
     AppLogger.initLogger("smartyPiLogger");
     AppLogger.debug("Starting SmartyPi4Home");
     
-    LCD lcd;
     try
     {
-      lcd = LCD.getInstance();
-      lcd.clear();
-      lcd.setText("SmartyPi4Home\nv0.1");
-      
-      ButtonPressedObserver buttonHandler = new ButtonPressedObserver(lcd);
-      Thread buttonCheckerThread = buttonHandler.addButtonListener(button -> 
+      final LCD lcd = LCD.getInstance();
+      try
+      {
+        lcd.clear();
+        lcd.setText("SmartyPi4Home\nv0.1");
+        Thread.sleep(1000);
+        
+        ButtonPressedObserver buttonHandler = new ButtonPressedObserver(lcd);
+        Thread buttonCheckerThread = buttonHandler.addButtonListener(button -> 
         System.out.println("Button: " + button.getPin()));
-      
-      GpioController gpio = GpioFactory.getInstance(); 
-      
-      GpioPinDigitalInput rxDataPin = 
-        gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
-      GpioPinListenerDigital rxStateChangeListener = 
-        e -> 
-          System.out.println("Pin 27 State changed: " + e.getState().getValue());
-      rxDataPin.addListener( rxStateChangeListener );
-      
-      //TODO tx GPIO == 00
-      
-      Runtime.getRuntime().addShutdownHook(
-        new Thread(() -> {System.out.println("Shutting down..." + lcd);}));
-      
-      //wait 
-      buttonCheckerThread.join();
+        
+        GpioController gpio = GpioFactory.getInstance(); 
+        
+        GpioPinDigitalInput rxDataPin = 
+          gpio.provisionDigitalInputPin(RaspiPin.GPIO_02, PinPullResistance.PULL_DOWN);
+        GpioPinListenerDigital rxStateChangeListener = 
+          e -> 
+        System.out.println("Pin 27 State changed: " + e.getState().getValue());
+        rxDataPin.addListener( rxStateChangeListener );
+        
+        //TODO tx GPIO == 00
+        
+        Runtime.getRuntime().addShutdownHook(
+          new Thread(() -> {System.out.println("Shutting down..." + lcd);}));
+        
+        //wait 
+        buttonCheckerThread.join();
+      }
+      catch (Exception e)
+      {
+        AppLogger.error("System Error: " + e.getMessage() + "\nStopping app...", e);
+        lcd.clear();
+        lcd.setText("System Error :(\nShutting down");
+        Thread.sleep(5000);
+        lcd.stop();
+        throw e;
+      }
     }
-    catch (Exception e)
+    catch (Exception e1)
     {
-      AppLogger.error("System Error: " + e.getMessage() + "\nStopping app...", e);
+      AppLogger.error("Unrecoverable system error: " + e1.getMessage(), e1);
       System.exit(1);
     }
   }
