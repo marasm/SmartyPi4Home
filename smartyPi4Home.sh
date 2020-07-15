f543f661ad281b722908a0fb27966cc8208a022c
Markas Korotkovas <mkorotkovas@yahoo.com>
2020-07-14 19:25:39 -0600

    reverted to needing sudo and running as process

--- smartyPi4Home.sh
+++ smartyPi4Home.sh
@@ -3,12 +3,10 @@
 echo Making sure the current working directory is correct
 cd "$(dirname "$0")" 
 
-export WIRINGPI_GPIOMEM=1
-
 echo Getting latest version from GitHub...
 git pull
 git submodule update --recursive --checkout --force --init
 
 echo Launching...
-gradle execute
-#nohup gradle execute > /dev/null 2>&1 &
+#gradle execute
+nohup gradle execute > /dev/null 2>&1 &
